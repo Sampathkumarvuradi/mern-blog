d69ec35dcb9a577226fb460db9625dd411074823
Sampath Kumar <sampathkumarvuradi@gmail.com>
2023-02-19 15:45:43 +0530

    Routing with react router dom

---
+++ client/src/Post.jsx
@@ -0,0 +1,29 @@
+export default function Post() {
+  return (
+    <div className='post'>
+      <div className='image'>
+        <img
+          src='https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png'
+          alt=''
+        />
+      </div>
+      <div className='texts'>
+        <h2>
+          Hot Box Your Uber? Zillow Rate Your Neighbor? A Designer Reimagines
+          Apps
+        </h2>
+        <p className='info'>
+          <a className='author'>Sampath Kumar</a>
+          <time>02:00 PM 19-02-2023</time>
+        </p>
+        <p className='summery'>
+          Back in 2015, the music streaming company launched Wrapped, a year-end
+          recap for each user that offered insights into their music listening
+          habits and the year’s most popular artists. Other brands began
+          borrowing the idea, a shameless if entertaining ploy to ratchet up
+          engagement.
+        </p>
+      </div>
+    </div>
+  );
+}
