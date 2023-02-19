3ab3256fdf2a64bc239dda17148fc536b4a04241
Sampath Kumar <sampathkumarvuradi@gmail.com>
2023-02-19 17:55:13 +0530

    Authentication

--- api/index.js
+++ api/index.js
@@ -1,14 +1,48 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
-app.use(cors());
+const salt = bcrypt.genSaltSync(10);
+const secret = "sdjhgskdghskdgaiegioagnoibg";
+
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+mongoose.connect(
+  "mongodb+srv://blog:bIYa7gFQWBbTihWk@cluster0.bifuist.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("Wrong Credentials");
+  }
 });
 
 app.listen(4000);
