2ceb908e26faf2034e8cecc3392b47499d4ed0cc
Sampath Kumar <sampathkumarvuradi@gmail.com>
2023-02-20 15:50:03 +0530

    Create new post page and adding it to database

--- api/index.js
+++ api/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const Post = require("./models/Post");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
+const multer = require("multer");
+const fs = require("fs");
 
 const app = express();
 dotenv.config();
+const uploadMiddleware = multer({ dest: "uploads/" });
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "sdjhgskdghskdgaiegioagnoibg";
@@ -60,6 +64,24 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { title, summery, content } = req.body;
+  const postDoc = await Post.create({
+    title,
+    summery,
+    content,
+    cover: newPath,
+  });
+
+  res.json(postDoc);
 });
 
 app.listen(4000);
